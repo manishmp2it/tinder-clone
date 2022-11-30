47391f0272fd7365c1a4f5e488e124ede576d68a
manishmp2it <manish.mp2it.mtr@gmail.com>
2022-11-30 11:57:14 +0530

    ok

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,39 +1,53 @@
 import { View, Text } from 'react-native'
 import React, { createContext, useContext } from 'react'
-import * as Google from "expo-google-app-auth"
+import * as WebBrowser from 'expo-web-browser';
+import * as Google from 'expo-auth-session/providers/google';
+
+WebBrowser.maybeCompleteAuthSession();
 
 
-const AuthContext=createContext({});
+const AuthContext = createContext({});
 
-const config={
-  androidClientId:'953808248579-fj2sium8q0fqhbvjhoufu7it7b3s9at2.apps.googleusercontent.com',
-  iosClientId:'953808248579-k6gdf4u0ke9gckppofierbrsokt7r4es.apps.googleusercontent.com',
-  webClientId:'953808248579-ir1gnrref3nudh1qtr550bjcb6cc6e2d.apps.googleusercontent.com',
-  scopes:["profile","email"],
-  permissions:["public_profile","email","gender","location"]
-}
+// const config = {
+//   expoClientId: 'GOOGLE_GUID.apps.googleusercontent.com',
+//   iosClientId: '953808248579-k6gdf4u0ke9gckppofierbrsokt7r4es.apps.googleusercontent.com',
+//   androidClientId: '953808248579-fj2sium8q0fqhbvjhoufu7it7b3s9at2.apps.googleusercontent.com',
+//   webClientId: '953808248579-ir1gnrref3nudh1qtr550bjcb6cc6e2d.apps.googleusercontent.com',
+//   // scopes:["profile","email"],
+//   // permissions:["public_profile","email","gender","location"]
+// }
 
-export const AuthProvider = ({children}) => {
+export const AuthProvider = ({ children }) => {
 
-    const signInwithGoogle = async()=>{
-        await Google.logInAsync(config).then(async (logInResult)=>{
-          if(logInResult.type=='success'){
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId: '953808248579-8eobvgg22tn7c409o0re6tc4dgm8hei3.apps.googleusercontent.com',
+    iosClientId: '953808248579-k6gdf4u0ke9gckppofierbrsokt7r4es.apps.googleusercontent.com',
+    androidClientId: '953808248579-fj2sium8q0fqhbvjhoufu7it7b3s9at2.apps.googleusercontent.com',
+    webClientId: '953808248579-8eobvgg22tn7c409o0re6tc4dgm8hei3.apps.googleusercontent.com',
+  });
 
-          }
-        });
-    }
-    
+  const signInwithGoogle = async () => {
+
+    promptAsync();
+
+    // await Google.useAuthRequest(config).then(async (logInResult) => {
+    //   if (logInResult.type == 'success') {
+
+    //   }
+    // });
+  }
+
   return (
     <AuthContext.Provider value={{
-        user:null,
-        signInwithGoogle
+      user: null,
+      signInwithGoogle
     }}>
       {children}
     </AuthContext.Provider>
   )
 }
 
-export default function useAuth(){
-    return useContext(AuthContext)
+export default function useAuth() {
+  return useContext(AuthContext)
 }
 

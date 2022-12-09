4155ff3c10efc27f48f487a085e04d018bfa5f96
manishmp2it <manish.mp2it.mtr@gmail.com>
2022-12-09 12:39:06 +0530

    ok

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -3,6 +3,7 @@ import React, { useLayoutEffect } from 'react'
 import useAuth from '../hooks/useAuth'
 import tw from 'twrnc';
 import { useNavigation } from '@react-navigation/native';
+import { AntDesign, MaterialIcons } from '@expo/vector-icons';
 
 const LoginScreen = () => {
   
@@ -18,8 +19,11 @@ const LoginScreen = () => {
   return (
     <View style={tw`flex-1`}>
       <ImageBackground resizeMode='cover' style={tw`flex-1`} source={{ uri: "https://tinder.com/static/tinder.png" }}>
-        <TouchableOpacity style={[tw`absolute bottom-30 w-52 bg-white p-4 rounded-2xl`,{marginHorizontal:"25%"}]} onPress={signInwithGoogle}>
-          <Text style={tw`font-semibold text-center`}>Sign in & get swiping</Text>
+        <TouchableOpacity style={[tw`flex-row items-center justify-evenly absolute bottom-50 w-70 bg-white p-4 rounded-2xl`,{marginHorizontal:"15%"}]} onPress={signInwithGoogle}>
+        <AntDesign name="google" size={24} color="black" /> <Text style={tw`font-semibold text-center`}>Sign in & get swiping</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[tw`flex-row items-center justify-evenly absolute bottom-30 w-70 bg-white p-4 rounded-2xl`,{marginHorizontal:"15%"}]} onPress={signInwithGoogle}>
+        <MaterialIcons name="mobile-screen-share" size={24} color="black" /> <Text style={tw` font-semibold text-center`}>Login with Mobile </Text>
         </TouchableOpacity>
       </ImageBackground>
     </View>
